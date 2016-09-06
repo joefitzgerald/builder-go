0f22072025e120b697cd65b1ef8cd8063141e2f7
lloiser <lukasberanek@gmx.net>
2016-09-07 00:31:45 +0200

    shortened the 'lint' method by moving code into separate functions; added test for duplicate errors

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -29,4 +29,34 @@ describe('builder-go', () => {
       expect(mainModule.getBuilder()).toBeTruthy()
     })
   })
+
+  describe('getMessages', () => {
+    it('ignores duplicate errors', () => {
+      let builder = mainModule.getBuilder()
+
+      // GIVEN the same results from both 'go install' and 'go test'
+      let outputs = [
+        {
+          'output': '# github.com/anonymous/sample-project\n.\\the-file.go:12: syntax error: unexpected semicolon or newline, expecting comma or }',
+          'linterName': 'build'
+        },
+        {
+          'output': '# github.com/anonymous/sample-project\n.\\the-file.go:12: syntax error: unexpected semicolon or newline, expecting comma or }',
+          'linterName': 'test'
+        }
+      ]
+
+      // WHEN I get the messages for these outputs
+      let messages = builder.getMessages(outputs, '/src/github.com/anonymous/sample-project')
+
+      // THEN I expect only one message to be returned because they are the same
+      expect(messages.length).toEqual(1)
+
+      let message = messages[0]
+      expect(message.name).toEqual('build')
+      expect(message.text.indexOf('syntax error: unexpected semicolon or newline, expecting comma or }') === 0).toBeTruthy()
+      expect(message.filePath).toEqual('/src/github.com/anonymous/sample-project/the-file.go')
+      expect(message.row).toEqual('12')
+    })
+  })
 })
